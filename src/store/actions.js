688d7931217d0464c94aff358abd6afa89e0abc7
Crowdbotics <team@crowdbotics.com>
2020-12-18 06:20:04 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,47 +2,57 @@ import * as types from "./constants"
 export const api_v1_customtext_list = () => ({
   type: types.API_V1_CUSTOMTEXT_LIST
 })
+
 export const api_v1_customtext_listSucceeded = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_LIST_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_customtext_listFailed = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_LIST_FAILED,
   response,
   starter
 })
+
 export const api_v1_customtext_read = () => ({
   type: types.API_V1_CUSTOMTEXT_READ
 })
+
 export const api_v1_customtext_readSucceeded = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_READ_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_customtext_readFailed = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_READ_FAILED,
   response,
   starter
 })
+
 export const api_v1_customtext_update = data => ({
   type: types.API_V1_CUSTOMTEXT_UPDATE,
   data
 })
+
 export const api_v1_customtext_updateSucceeded = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_customtext_updateFailed = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_UPDATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_customtext_partial_update = data => ({
   type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE,
   data
 })
+
 export const api_v1_customtext_partial_updateSucceeded = (
   response,
   starter
@@ -51,207 +61,250 @@ export const api_v1_customtext_partial_updateSucceeded = (
   response,
   starter
 })
+
 export const api_v1_customtext_partial_updateFailed = (response, starter) => ({
   type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_list = () => ({ type: types.API_V1_HGFJGJH_LIST })
 export const api_v1_hgfjgjh_listSucceeded = (response, starter) => ({
   type: types.API_V1_HGFJGJH_LIST_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_listFailed = (response, starter) => ({
   type: types.API_V1_HGFJGJH_LIST_FAILED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_create = data => ({
   type: types.API_V1_HGFJGJH_CREATE,
   data
 })
+
 export const api_v1_hgfjgjh_createSucceeded = (response, starter) => ({
   type: types.API_V1_HGFJGJH_CREATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_createFailed = (response, starter) => ({
   type: types.API_V1_HGFJGJH_CREATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_read = () => ({ type: types.API_V1_HGFJGJH_READ })
 export const api_v1_hgfjgjh_readSucceeded = (response, starter) => ({
   type: types.API_V1_HGFJGJH_READ_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_readFailed = (response, starter) => ({
   type: types.API_V1_HGFJGJH_READ_FAILED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_update = data => ({
   type: types.API_V1_HGFJGJH_UPDATE,
   data
 })
+
 export const api_v1_hgfjgjh_updateSucceeded = (response, starter) => ({
   type: types.API_V1_HGFJGJH_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_updateFailed = (response, starter) => ({
   type: types.API_V1_HGFJGJH_UPDATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_partial_update = data => ({
   type: types.API_V1_HGFJGJH_PARTIAL_UPDATE,
   data
 })
+
 export const api_v1_hgfjgjh_partial_updateSucceeded = (response, starter) => ({
   type: types.API_V1_HGFJGJH_PARTIAL_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_partial_updateFailed = (response, starter) => ({
   type: types.API_V1_HGFJGJH_PARTIAL_UPDATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_delete = () => ({
   type: types.API_V1_HGFJGJH_DELETE
 })
+
 export const api_v1_hgfjgjh_deleteSucceeded = (response, starter) => ({
   type: types.API_V1_HGFJGJH_DELETE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_hgfjgjh_deleteFailed = (response, starter) => ({
   type: types.API_V1_HGFJGJH_DELETE_FAILED,
   response,
   starter
 })
+
 export const api_v1_homepage_list = () => ({ type: types.API_V1_HOMEPAGE_LIST })
 export const api_v1_homepage_listSucceeded = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_LIST_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_homepage_listFailed = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_LIST_FAILED,
   response,
   starter
 })
+
 export const api_v1_homepage_read = () => ({ type: types.API_V1_HOMEPAGE_READ })
 export const api_v1_homepage_readSucceeded = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_READ_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_homepage_readFailed = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_READ_FAILED,
   response,
   starter
 })
+
 export const api_v1_homepage_update = data => ({
   type: types.API_V1_HOMEPAGE_UPDATE,
   data
 })
+
 export const api_v1_homepage_updateSucceeded = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_homepage_updateFailed = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_UPDATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_homepage_partial_update = data => ({
   type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE,
   data
 })
+
 export const api_v1_homepage_partial_updateSucceeded = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_homepage_partial_updateFailed = (response, starter) => ({
   type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_login_create = () => ({ type: types.API_V1_LOGIN_CREATE })
 export const api_v1_login_createSucceeded = (response, starter) => ({
   type: types.API_V1_LOGIN_CREATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_login_createFailed = (response, starter) => ({
   type: types.API_V1_LOGIN_CREATE_FAILED,
   response,
   starter
 })
+
 export const api_v1_signup_create = data => ({
   type: types.API_V1_SIGNUP_CREATE,
   data
 })
+
 export const api_v1_signup_createSucceeded = (response, starter) => ({
   type: types.API_V1_SIGNUP_CREATE_SUCCEEDED,
   response,
   starter
 })
+
 export const api_v1_signup_createFailed = (response, starter) => ({
   type: types.API_V1_SIGNUP_CREATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_login_create = data => ({
   type: types.REST_AUTH_LOGIN_CREATE,
   data
 })
+
 export const rest_auth_login_createSucceeded = (response, starter) => ({
   type: types.REST_AUTH_LOGIN_CREATE_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_login_createFailed = (response, starter) => ({
   type: types.REST_AUTH_LOGIN_CREATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_logout_list = () => ({
   type: types.REST_AUTH_LOGOUT_LIST
 })
+
 export const rest_auth_logout_listSucceeded = (response, starter) => ({
   type: types.REST_AUTH_LOGOUT_LIST_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_logout_listFailed = (response, starter) => ({
   type: types.REST_AUTH_LOGOUT_LIST_FAILED,
   response,
   starter
 })
+
 export const rest_auth_logout_create = () => ({
   type: types.REST_AUTH_LOGOUT_CREATE
 })
+
 export const rest_auth_logout_createSucceeded = (response, starter) => ({
   type: types.REST_AUTH_LOGOUT_CREATE_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_logout_createFailed = (response, starter) => ({
   type: types.REST_AUTH_LOGOUT_CREATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_password_change_create = data => ({
   type: types.REST_AUTH_PASSWORD_CHANGE_CREATE,
   data
 })
+
 export const rest_auth_password_change_createSucceeded = (
   response,
   starter
@@ -260,15 +313,18 @@ export const rest_auth_password_change_createSucceeded = (
   response,
   starter
 })
+
 export const rest_auth_password_change_createFailed = (response, starter) => ({
   type: types.REST_AUTH_PASSWORD_CHANGE_CREATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_password_reset_create = data => ({
   type: types.REST_AUTH_PASSWORD_RESET_CREATE,
   data
 })
+
 export const rest_auth_password_reset_createSucceeded = (
   response,
   starter
@@ -277,15 +333,18 @@ export const rest_auth_password_reset_createSucceeded = (
   response,
   starter
 })
+
 export const rest_auth_password_reset_createFailed = (response, starter) => ({
   type: types.REST_AUTH_PASSWORD_RESET_CREATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_password_reset_confirm_create = data => ({
   type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE,
   data
 })
+
 export const rest_auth_password_reset_confirm_createSucceeded = (
   response,
   starter
@@ -294,6 +353,7 @@ export const rest_auth_password_reset_confirm_createSucceeded = (
   response,
   starter
 })
+
 export const rest_auth_password_reset_confirm_createFailed = (
   response,
   starter
@@ -302,24 +362,29 @@ export const rest_auth_password_reset_confirm_createFailed = (
   response,
   starter
 })
+
 export const rest_auth_registration_create = data => ({
   type: types.REST_AUTH_REGISTRATION_CREATE,
   data
 })
+
 export const rest_auth_registration_createSucceeded = (response, starter) => ({
   type: types.REST_AUTH_REGISTRATION_CREATE_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_registration_createFailed = (response, starter) => ({
   type: types.REST_AUTH_REGISTRATION_CREATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_registration_verify_email_create = data => ({
   type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE,
   data
 })
+
 export const rest_auth_registration_verify_email_createSucceeded = (
   response,
   starter
@@ -328,6 +393,7 @@ export const rest_auth_registration_verify_email_createSucceeded = (
   response,
   starter
 })
+
 export const rest_auth_registration_verify_email_createFailed = (
   response,
   starter
@@ -336,40 +402,48 @@ export const rest_auth_registration_verify_email_createFailed = (
   response,
   starter
 })
+
 export const rest_auth_user_read = () => ({ type: types.REST_AUTH_USER_READ })
 export const rest_auth_user_readSucceeded = (response, starter) => ({
   type: types.REST_AUTH_USER_READ_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_user_readFailed = (response, starter) => ({
   type: types.REST_AUTH_USER_READ_FAILED,
   response,
   starter
 })
+
 export const rest_auth_user_update = data => ({
   type: types.REST_AUTH_USER_UPDATE,
   data
 })
+
 export const rest_auth_user_updateSucceeded = (response, starter) => ({
   type: types.REST_AUTH_USER_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_user_updateFailed = (response, starter) => ({
   type: types.REST_AUTH_USER_UPDATE_FAILED,
   response,
   starter
 })
+
 export const rest_auth_user_partial_update = data => ({
   type: types.REST_AUTH_USER_PARTIAL_UPDATE,
   data
 })
+
 export const rest_auth_user_partial_updateSucceeded = (response, starter) => ({
   type: types.REST_AUTH_USER_PARTIAL_UPDATE_SUCCEEDED,
   response,
   starter
 })
+
 export const rest_auth_user_partial_updateFailed = (response, starter) => ({
   type: types.REST_AUTH_USER_PARTIAL_UPDATE_FAILED,
   response,
